3974d308ab238c1a22d26cd873baaf1f2a1cbfa8
OnionJumoke <onionjumoke@gmail.com>
2025-09-28 05:38:27 -0300

    linksfix 2

--- depictions/links.js
+++ depictions/links.js
@@ -1,19 +1,9 @@
-// script taken from https://www.bennadel.com/blog/2302-preventing-links-in-standalone-iphone-applications-from-opening-in-mobile-safari.htm
-// Listen for ALL links at the top level of the document. For
-// testing purposes, we're not going to worry about LOCAL vs.
-// EXTERNAL links - we'll just demonstrate the feature.
-$( document ).on(
-	"click",
-	"a",
-	function( event ){
-
-		// Stop the default behavior of the browser, which
-		// is to change the URL of the page.
-		event.preventDefault();
-
-		// Manually change the location of the page to stay in
-		// "Standalone" mode and change the URL at the same time.
-		location.href = $( event.target ).attr( "href" );
-
-	}
-);
+var a=document.getElementsByTagName("a");
+for(var i=0;i<a.length;i++)
+{
+    a[i].onclick=function()
+    {
+        window.location=this.getAttribute("href");
+        return false
+    }
+}
